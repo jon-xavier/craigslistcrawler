08299f93c805e8544b3b9fda9a4aa1b456f82c03
Jon <jonathan.xavier0@gmail.com>
2015-10-09 21:50:08 -0700

    sort of working now

--- craigslistscraper/craigslistscraper/spiders/__init__.py
+++ craigslistscraper/craigslistscraper/spiders/__init__.py
@@ -3,8 +3,8 @@
 # Please refer to the documentation for information on how to create and manage
 # your spiders.
 from scrapy.spiders import CrawlSpider, Rule 
-from scrapy.selector import Selector, HtmlXPathSelector
-from craigslistscraper.craigslistscraper.items import CraigslistscraperItem
+from scrapy.selector import Selector
+from craigslistscraper.items import CraigslistscraperItem
 from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
 
 keys = ['marketing', 'journalism', 'journalist', 'copywriting', 'communications', 'copywriter']
@@ -14,17 +14,13 @@ class JobsFinder(CrawlSpider):
     allowed_domains = ["craigslist.org"]
     start_urls = ["http://sfbay.craigslist.org/search/jjj"]
 
-    rules = (
-            (Rule(SgmlLinkExtractor(allow=(), restrict_xpaths=(//span[@class='buttons']/a[@class='button next'],)), callback="parse_items", follow= True),
-            )
 
-    def parse_items(self, response):
-        item = CraigslistscraperItem
-        hxs = HtmlXPathSelector(response)
+    def parse(self, response):
+        item = CraigslistscraperItem()
+        hxs = Selector(response)
         if any(key in response.body for key in keys):
             
             item['name'] = hxs.xpath('//span[@class="pl"]')
             item['url'] = response.url
-            item['postbody'] = hxs.xpath("//section[@class='body']")
-            yield item 
-        else pass       
+            item['post_body'] = hxs.xpath("//section[@class='body']")
+            yield item  
