11ae764590659a73af9874fd2b6d067af70e6ada
Jonathan Xavier <jonathan.xavier@jonathan.xavier-00250>
2015-10-10 10:35:09 -0700

    user agents added

---
+++ craigslistscraper/craigslistscraper/spiders/__init__.py
@@ -0,0 +1,26 @@
+# This package will contain the spiders of your Scrapy project
+#
+# Please refer to the documentation for information on how to create and manage
+# your spiders.
+from scrapy.spiders import CrawlSpider, Rule 
+from scrapy.selector import Selector
+from craigslistscraper.items import CraigslistscraperItem
+from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
+
+keys = ['marketing', 'journalism', 'journalist', 'copywriting', 'communications', 'copywriter']
+
+class JobsFinder(CrawlSpider):
+    name = "jobsfinder"
+    allowed_domains = ["craigslist.org"]
+    start_urls = ["http://sfbay.craigslist.org/search/jjj"]
+    custom_settings = [user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36"]
+
+    def parse(self, response):
+        item = CraigslistscraperItem()
+        hxs = Selector(response)
+        if any(key in response.body for key in keys):
+            
+            item['name'] = hxs.xpath('//span[@class="pl"]')
+            item['url'] = response.url
+            item['post_body'] = hxs.xpath("//section[@class='body']")
+            yield item  
